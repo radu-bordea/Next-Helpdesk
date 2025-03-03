c35f3413da96d65ed00e7c00202a76328de6e831
radu-bordea <radu.bordea.dev@gmail.com>
2025-03-03 13:59:41 +0100

    add routes api

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - npm install react-icons --save
 - npm install json-server
 # run json-server
-- npx json-server --watch --port 4000 ./_data/db.json
-- npx json-server --port 4000 ./_data/db.json
+- npx json-server --watch --port 3000 ./_data/db.json
+- npx json-server --port 3000 ./_data/db.json
 
 - npm run build
---
+++ app/api/tickets/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const response = await fetch("http://localhost:3000/tickets");
+
+  const tickets = await res.json();
+
+  return NextResponse.json(tickets, {
+    status: 200,
+  });
+}
+
+export async function POST(request) {
+  const ticket = await request.json();
+
+  const res = await fetch("http://localhost:3000/tickets", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(ticket),
+  });
+
+  const newTicket = await res.json();
+
+  return NextResponse.json(newTicket, { status: 201 });
+}
